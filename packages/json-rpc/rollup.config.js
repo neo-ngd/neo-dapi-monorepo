6fae97f382217055d98f68b119b78bb6cfea9b3e
liuqiang <liuqiang1357@163.com>
2021-08-04 14:21:44 +0800

    simplify config for umd

--- packages/json-rpc/rollup.config.js
+++ packages/json-rpc/rollup.config.js
@@ -1,9 +1,5 @@
 import path from 'path';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import pkg from './package.json';
 
 const input = './src/index.ts';
@@ -14,27 +10,21 @@ export default [
   {
     input,
     external,
-    plugins: [
-      typescript(),
-      json(), // so Rollup can import json files
-      nodePolyfills(), // node built-in modules pollyfill for browser
-      resolve({ preferBuiltins: false }), // so Rollup can find dependencies
-      commonjs(), // so Rollup can convert dependencies to an ES module
-    ],
+    plugins: [typescript()],
     output: { name: 'jsonRpc', dir: path.dirname(pkg.browser), format: 'umd', sourcemap: true },
   },
   // CommonJS (for Node)
   {
     input,
     external,
-    plugins: [typescript(), json()],
+    plugins: [typescript()],
     output: { dir: path.dirname(pkg.main), format: 'cjs', sourcemap: true },
   },
   // ES module (for bundlers) build.
   {
     input,
     external,
-    plugins: [typescript(), json()],
+    plugins: [typescript()],
     output: { dir: path.dirname(pkg.module), format: 'es', sourcemap: true },
   },
 ];
