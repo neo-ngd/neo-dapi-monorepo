74bad2444f11b1b9dd06ce44f6175fbd3e66f4ef
liuqiang <liuqiang1357@163.com>
2021-08-05 01:46:31 +0800

    allow pass url to NeoDapi constructor

--- packages/neo-dapi/src/index.ts
+++ packages/neo-dapi/src/index.ts
@@ -1,16 +1,22 @@
 import { NeoDapiFura } from '@neongd/neo-dapi-fura';
 import { NeoDapiNode } from '@neongd/neo-dapi-node';
 import { NeoDapiWallet } from '@neongd/neo-dapi-wallet';
-import { INeoProvider } from '@neongd/neo-provider';
+import { INeoProvider, JsonRpcNeoProvider } from '@neongd/neo-provider';
 
 export class NeoDapi {
   node: NeoDapiNode;
   wallet: NeoDapiWallet;
   fura: NeoDapiFura;
 
-  constructor(provider: INeoProvider) {
-    this.node = new NeoDapiNode(provider);
-    this.wallet = new NeoDapiWallet(provider);
-    this.fura = new NeoDapiFura(provider);
+  constructor(provider: INeoProvider | string) {
+    let finalProvider: INeoProvider;
+    if (typeof provider === 'string') {
+      finalProvider = new JsonRpcNeoProvider(provider);
+    } else {
+      finalProvider = provider;
+    }
+    this.node = new NeoDapiNode(finalProvider);
+    this.wallet = new NeoDapiWallet(finalProvider);
+    this.fura = new NeoDapiFura(finalProvider);
   }
 }
