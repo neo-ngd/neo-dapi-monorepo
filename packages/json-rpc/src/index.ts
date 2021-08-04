2d000e7cadb79e356dc51e2e9b3d92288beb5dc3
liuqiang <liuqiang1357@163.com>
2021-08-04 10:05:28 +0800

    export http connection and ws connection

--- packages/json-rpc/src/index.ts
+++ packages/json-rpc/src/index.ts
@@ -5,4 +5,6 @@ export * from './formatters';
 export * from './validators';
 export * from './postMessageConnection';
 export * from './url';
+export * from './httpConnection';
+export * from './wsConnection';
 export * from './jsonRpcProxy';
