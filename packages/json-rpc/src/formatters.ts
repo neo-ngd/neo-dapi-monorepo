eacf53fb1cda2f02e9cc8c0f00ffd0b04d86e9d5
liuqiang <liuqiang1357@163.com>
2021-08-12 14:50:12 +0800

    change payloadId generation algorithm

--- packages/json-rpc/src/formatters.ts
+++ packages/json-rpc/src/formatters.ts
@@ -8,10 +8,10 @@ import {
   JsonRpcResult,
 } from './types';
 
+let lastPayloadId = 0;
+
 export function payloadId(): number {
-  const date = Date.now() * Math.pow(10, 3);
-  const extra = Math.floor(Math.random() * Math.pow(10, 3));
-  return date + extra;
+  return ++lastPayloadId;
 }
 
 export function formatJsonRpcRequest<T = any>(
