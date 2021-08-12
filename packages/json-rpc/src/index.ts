5841e4d53acb895c60c1a41db1150b89dbd33dc2
liuqiang <liuqiang1357@163.com>
2021-08-12 16:50:38 +0800

    export json

--- packages/json-rpc/src/index.ts
+++ packages/json-rpc/src/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './errors';
 export * from './formatters';
 export * from './validators';
+export * from './json';
 export * from './postMessageConnection';
 export * from './url';
 export * from './httpConnection';
