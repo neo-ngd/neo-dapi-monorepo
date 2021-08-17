be7977e1dcbabd361b7e69111aaa9615345817b8
liuqiang <liuqiang1357@163.com>
2021-08-17 12:11:58 +0800

    rename errors export name

--- packages/neo-dapi-wallet/src/errors.ts
+++ packages/neo-dapi-wallet/src/errors.ts
@@ -1,18 +1,18 @@
 import { ErrorResponse, getError as getStandardError } from '@neongd/json-rpc';
 
-export enum DapiWalletErrorCodes {
+export enum ErrorCodes {
   UnsupportedNetwork = 100,
 }
 
 const ERROR_MESSAGE_MAP = {
-  [DapiWalletErrorCodes.UnsupportedNetwork]: 'Unsupported network.',
+  [ErrorCodes.UnsupportedNetwork]: 'Unsupported network.',
 };
 
-export function getDapiError(code: number): ErrorResponse {
-  if (code in DapiWalletErrorCodes) {
+export function getError(code: number): ErrorResponse {
+  if (code in ErrorCodes) {
     return {
       code,
-      message: ERROR_MESSAGE_MAP[code as DapiWalletErrorCodes],
+      message: ERROR_MESSAGE_MAP[code as ErrorCodes],
     };
   }
   return getStandardError(code);
