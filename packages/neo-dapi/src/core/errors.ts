478bbe7d24e0bf48415b7aa1664249c113a91a68
liuqiang <liuqiang1357@163.com>
2021-09-28 18:05:41 +0800

    add neo dapi error codes

--- packages/neo-dapi/src/core/errors.ts
+++ packages/neo-dapi/src/core/errors.ts
@@ -3,11 +3,23 @@ import { ErrorResponse } from '@neongd/json-rpc';
 export enum NeoDapiErrorCodes {
   InternalError = 100,
   UnsupportedNetwork = 101,
+  NoAccount = 102,
+  IncorrectNetwork = 103,
+  MalformedInput = 104,
+  InsufficientFunds = 105,
+  RemoteRpcError = 106,
+  UserRejected = 107,
 }
 
 const ERROR_MESSAGE_MAP = {
   [NeoDapiErrorCodes.InternalError]: 'Internal error',
   [NeoDapiErrorCodes.UnsupportedNetwork]: 'Unsupported network',
+  [NeoDapiErrorCodes.NoAccount]: 'NoAccount',
+  [NeoDapiErrorCodes.IncorrectNetwork]: 'Incorrect Network',
+  [NeoDapiErrorCodes.MalformedInput]: 'Malformed Input',
+  [NeoDapiErrorCodes.InsufficientFunds]: 'Insufficient Funds',
+  [NeoDapiErrorCodes.RemoteRpcError]: 'Remote Rpc Error',
+  [NeoDapiErrorCodes.UserRejected]: 'User Rejected',
 };
 
 export function isNeoDapiErrorCode(code: number): code is NeoDapiErrorCodes {
