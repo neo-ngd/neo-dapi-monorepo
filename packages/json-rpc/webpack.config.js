385417e95c66c526d062791f8c7f465607fe46a3
liuqiang <liuqiang1357@163.com>
2021-10-18 18:10:02 +0800

    migrate build tools from rollup to webpack

---
+++ packages/json-rpc/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.resolve(__dirname, 'dist', 'cjs', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'umd'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'jsonRpc',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+  resolve: {
+    fallback: {
+      process: false,
+      net: false,
+      zlib: false,
+      stream: false,
+      tls: false,
+      crypto: false,
+      http: false,
+      https: false,
+      fs: false,
+      path: false,
+      os: false,
+      url: false,
+    },
+  },
+  optimization: {
+    minimize: false,
+  },
+};
