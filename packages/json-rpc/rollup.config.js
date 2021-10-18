6645ef9495bdb3cc088aa789d13736f716a85347
liuqiang <liuqiang1357@163.com>
2021-10-18 15:45:50 +0800

    move all umd dependencies to external

--- packages/json-rpc/rollup.config.js
+++ packages/json-rpc/rollup.config.js
@@ -1,10 +1,5 @@
 import path from 'path';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import globals from 'rollup-plugin-node-globals';
-import polyfill from 'rollup-plugin-polyfill-node';
 import pkg from './package.json';
 
 const input = './src/index.ts';
@@ -14,14 +9,8 @@ export default [
   // browser-friendly UMD build
   {
     input,
-    plugins: [
-      typescript(),
-      json(), // so Rollup can import json files
-      resolve({ preferBuiltins: false }), // so Rollup can find dependencies
-      commonjs(), // so Rollup can convert dependencies to an ES module
-      globals(), // insert node globals including so code that works with browserify
-      polyfill(), // node built-in modules pollyfill for browser
-    ],
+    external,
+    plugins: [typescript()],
     output: { name: 'jsonRpc', dir: path.dirname(pkg.browser), format: 'umd', sourcemap: true },
   },
   // CommonJS (for Node)
